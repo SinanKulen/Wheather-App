84002a314dcf89158ad5921e4fa35df2245b6561
Sinan Kulen <sinan.kulen@gmail.com>
2022-02-18 16:09:37 +0300

    Initial Commit

---
+++ Wheather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheather App
+//
+//  Created by Sinan Kulen on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
